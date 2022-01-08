106aadeb8a5ba7d537499ce7f2c064d1c0743610
Logan Ross <logo3102@gmail.com>
2022-01-08 00:54:38 -0800

    Working prototype

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import os
-
-server_address = r"\\10.1.10.52\d\ "   #location of remote server
+import subprocess
+server_address = '\\\\10.1.10.52\\d\\'   #location of remote server
 
 app_type = "java"               #app type:  java, exe
 app_name = "server.jar"         #
 app_dir = "1.18\\"
 app_params = "-Xmx8G"
 
-print(server_address)
+print(server_address+app_dir)
 def StartApp():
     if app_type == "java":
-        os.popen("java " + app_params + " -jar " + server_address + app_dir + app_name)
+        os.system('powershell -Command "cd ' + server_address + app_dir + ' ; java ' + app_params + ' ' '-jar ' + app_name + '"')
 
 
 StartApp()
