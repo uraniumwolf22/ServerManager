75512be2f3c169257a5fb3840afb82fad8ebe300
Logan Ross <logo3102@gmail.com>
2022-01-08 00:16:42 -0800

    test1

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import os
 
+server_address = r"\\10.1.10.52\d\ "   #location of remote server
+
+app_type = "java"               #app type:  java, exe
+app_name = "server.jar"         #
+app_dir = "1.18\\"
+app_params = "-Xmx8G"
+
+print(server_address)
+def StartApp():
+    if app_type == "java":
+        os.popen("java " + app_params + " -jar " + server_address + app_dir + app_name)
+
+
+StartApp()
